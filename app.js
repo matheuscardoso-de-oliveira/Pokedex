01189ff9c071d7b6e1ffe43099a46fc7e1660588
Matheuscardoso-de-oliveira <mateusoliveira109@gmail.com>
2025-08-27 11:51:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+fetch('https://pokeapi.co/api/v2/pokemon?limit=10',{method:'GET'})
 .then(response => response.json())
 .then(allpokemon => console.log(allpokemon))
