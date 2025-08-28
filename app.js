d43c1e0d9a2acfea34fc70e928bf538ce8996ece
Matheuscardoso-de-oliveira <mateusoliveira109@gmail.com>
2025-08-28 11:11:57 -0300

    updates

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 fetch('https://pokeapi.co/api/v2/pokemon?limit=10',{method:'GET'})
 .then(response => response.json())
-.then(allpokemon => console.log(allpokemon))
+.then((allpokemon) => {
+
+    var pokemons = [];
+
+    allpokemon.results.map((val)=>{
+        fetch(val.url,{method:'GET'})
+        .then(response => response.json())
+        .then((pokemonSingle)=>{
+            pokemons.push({nome:val.name, imagem: pokemonSingle.sprites.front_default})
+            if(pokemons.length == 10){
+                console.log(pokemons)
+            }
+            
+        })
+
+
+
+    })
+
+}
+
+)
