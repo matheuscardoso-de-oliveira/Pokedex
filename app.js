fd460bcf303fe86c11fecbeba9beb0a8327bb3b6
Matheuscardoso-de-oliveira <mateusoliveira109@gmail.com>
2025-08-28 11:46:08 -0300

    updates

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-fetch('https://pokeapi.co/api/v2/pokemon?limit=10',{method:'GET'})
+var quantidade = document.getElementById('quantidade')
+quantidade.addEventListener('change', ()=>{
+    catchAll(quantidade.value)
+})
+catchAll(151)
+function catchAll(quantidade){
+fetch('https://pokeapi.co/api/v2/pokemon?limit='+quantidade,{method:'GET'})
 .then(response => response.json())
 .then((allpokemon) => {
 
@@ -9,25 +15,25 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=10',{method:'GET'})
         .then(response => response.json())
         .then((pokemonSingle)=>{
             pokemons.push({nome:val.name, imagem: pokemonSingle.sprites.front_default})
-            if(pokemons.length == 10){
+            if(pokemons.length == quantidade){
                 //console.log(pokemons)
                 var pokemonBoxes = document.querySelector('.pokemon-boxes')
                 pokemonBoxes.innerHTML= ""
                 pokemons.map(function(val){
-                   
+                   pokemonBoxes.innerHTML+= 
+                   `<div class="pokemon-box">
+                    <img src="${val.imagem}" alt="${val.nome}">
+                    <p>${val.nome}</p>
+                    </div>`
                    /* <div class="pokemon-box">
                     <img src="" alt="">
                     <p></p>
                     </div>*/
                 })
             }
-            
         })
-
-
-
     })
   
 }
-
 )
+}
