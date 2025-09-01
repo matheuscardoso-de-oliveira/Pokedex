c2209d79214376f50f4087194008cf57ecd3fed3
Matheuscardoso-de-oliveira <mateusoliveira109@gmail.com>
2025-09-01 11:45:42 -0300

    updates

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit='+quantidade,{method:'GET'})
     var pokemons = [];
 
     allpokemon.results.map((val)=>{
+        
         fetch(val.url,{method:'GET'})
         .then(response => response.json())
         .then((pokemonSingle)=>{
-            pokemons.push({nome:val.name, imagem: pokemonSingle.sprites.front_default})
+            console.log(pokemonSingle)
+            pokemons.push({nome:pokemonSingle.name, imagem: pokemonSingle.sprites.front_default, abilidade: pokemonSingle.abilities, dex: pokemonSingle.game_indices[3].game_index, exp: pokemonSingle.base_experience})
+           
             if(pokemons.length == quantidade){
                 //console.log(pokemons)
                 var pokemonBoxes = document.querySelector('.pokemon-boxes')
@@ -23,8 +26,12 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit='+quantidade,{method:'GET'})
                    pokemonBoxes.innerHTML+= 
                    `<div class="pokemon-box">
                     <img src="${val.imagem}" alt="${val.nome}">
-                    <p>${val.nome}</p>
+                    <p>Nome: ${val.nome}</p>
+                    <p>NationalDex: ${val.dex}</p>
+                   
+
                     </div>`
+                    
                    /* <div class="pokemon-box">
                     <img src="" alt="">
                     <p></p>
