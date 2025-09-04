167f4df15c1ce96bb8ec3a1e8c1643f6bc6e1b86
Matheuscardoso-de-oliveira <mateusoliveira109@gmail.com>
2025-09-04 11:55:26 -0300

    updates

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit='+1000,{method:'GET'})
         fetch(val.url)
         .then(response => response.json())
         .then((pokemonSingle)=>{
-           // console.log(pokemonSingle)
+           console.log(pokemonSingle)
             pokemons.push({nome:pokemonSingle.name, imagem: pokemonSingle.sprites.front_default, abilidade: pokemonSingle.abilities,  exp: pokemonSingle.base_experience})
            
             if(pokemons.length == quantidade){
@@ -39,7 +39,7 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit='+1000,{method:'GET'})
                    texto += `${abi.ability.name} `
                    })
 
-                    texto+= `</p> 
+                    texto+= `</p>
                     </div> 
                     </div>`
                     pokemonBoxes.innerHTML+= texto
